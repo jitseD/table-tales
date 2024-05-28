09891c5c59e7421341eba2767de3f6baf0d97f3d
Jitse Dekeyser <jitse.dekeyser@student.howest.be>
2024-05-28 11:35:19 +0200

    multi-phone-screen: socket rooms

--- testing/01-multi-phone-screen/public/js/script.js
+++ testing/01-multi-phone-screen/public/js/script.js
@@ -2,8 +2,51 @@ let socket;
 let socketIds = [];
 let $cursor
 
-const $myId = document.querySelector(`.myId`);
-const $otherIds = document.querySelector(`.otherIds`);
+const $myId = document.querySelector(`.my__id`);
+const $otherIds = document.querySelector(`.other__ids`);
+const $initiateDance = document.querySelector(`.initiate__dance`);
+const $createDance = document.querySelector(`.create__dance`);
+const $joinDance = document.querySelector(`.join__dance`);
+const $danceLobby = document.querySelector(`.lobby__dance`);
+const $danceCode = document.querySelector(`.dance__code`);
+const $formDance = document.querySelector(`.form__dance`);
+const $danceForm = document.querySelector(`.dance__form`);
+const $danceCodeInput = document.querySelector(`.dance__code--input`);
+
+const createDanceHandle = () => {
+    socket.emit(`hostDance`, 6);
+    socket.on(`danceCode`, (code) => {
+        $initiateDance.classList.add(`hide`);
+        $danceLobby.classList.remove(`hide`);
+        $danceCode.textContent = code;
+    })
+}
+
+const joinDanceHandle = () => {
+    $initiateDance.classList.add(`hide`);
+    $formDance.classList.remove(`hide`);
+}
+
+const danceFormSubmitHandle = e => {
+    e.preventDefault();
+    const danceCode = $danceCodeInput.value.trim();
+    if (danceCode !== ``) {
+        socket.emit(`joinDance`, danceCode);
+    } else {
+        console.log($danceCodeInput.value);
+        console.log(`please enter a dance code`);
+    }
+
+    socket.on(`joinedDance`, (code) => {
+        $danceForm.classList.add(`hide`);
+        $danceLobby.classList.remove(`hide`);
+        $danceCode.textContent = code;
+    })
+
+    socket.on(`invalidCode`, () => {
+        console.log(`code not valid: please enter a valis dance code`);
+    })
+}
 
 const init = () => {
     socket = io.connect(`/`);
@@ -12,18 +55,22 @@ const init = () => {
     });
 
     socket.on(`clientList`, (clientIds) => {
-        $otherIds.innerHTML = '';
-        clientIds.forEach(clientId => {
-            console.log(`socket.id - ` + socket.id);
-            console.log(`clientId - ` + clientId);
-            if (clientId[0] !== socket.id) {
-                const listItem = document.createElement('li');
-                listItem.textContent = clientId[0];
-                $otherIds.appendChild(listItem);
+        $otherIds.innerHTML = ``;
+        for (const otherSocetId in clientIds) {
+            if (clientIds.hasOwnProperty(otherSocetId)) {
+                const clientId = clientIds[otherSocetId]
+                if (clientId !== socket.id) {
+                    const listItem = document.createElement(`li`);
+                    listItem.textContent = clientId;
+                    $otherIds.appendChild(listItem);
+                }
             }
-        });
+        }
     })
 
+    $createDance.addEventListener(`click`, createDanceHandle);
+    $joinDance.addEventListener(`click`, joinDanceHandle);
+    $danceForm.addEventListener(`submit`, danceFormSubmitHandle);
 };
 
 init();
--- testing/01-multi-phone-screen/server.js
+++ testing/01-multi-phone-screen/server.js
@@ -12,23 +12,60 @@ const server = https.createServer(options, app);
 const { Server } = require("socket.io");
 const io = new Server(server);
 
-const port = 3000;
-const clients = {};
+const port = 443;
+const danceCodes = {};
 
 app.use(express.static('public'))
 server.listen(port, () => {
     console.log(`App listening on port ${port}`)
 })
 
+const generateRandomCode = (codeLength) => {
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+    let code = '';
+
+    for (let i = 0; i < codeLength; i++) {
+        const randomIndex = Math.floor(Math.random() * characters.length);
+        code += characters.charAt(randomIndex);
+    }
+
+    return code;
+}
+
 io.on('connection', socket => {
     console.log(`Connection`);
 
-    clients[socket.id] = socket.id;
+    socket.on('hostDance', (codeLength) => {
+        let code;
+        do {
+            code = generateRandomCode(codeLength);
+        } while (danceCodes[code]);
+        
+        danceCodes[code] = [socket.id];
+        socket.join(code);
+        socket.emit('danceCode', code);
+    });
 
-    io.emit(`clientList`, Object.entries(clients));
+    socket.on('joinDance', code => {
+        if (danceCodes[code]) {
+            danceCodes[code].push(socket.id);
+            socket.join(code);
+            socket.emit('joinedDance', code);
+            io.to(code).emit('clientList', danceCodes[code]);
+        } else {
+            socket.emit('invalidCode');
+        }
+    });
     
     socket.on('disconnect', () => {
-        delete clients[socket.id];
-        io.emit(`clientList`, Object.entries(clients));
-    })
+        for (const code in danceCodes) {
+            danceCodes[code] = danceCodes[code].filter(id => id !== socket.id);
+            socket.leave(code);
+            if (danceCodes[code].length === 0) {
+                delete danceCodes[code];
+            } else {
+                io.to(code).emit('clientList', danceCodes[code]);
+            }
+        }
+    });
 });
