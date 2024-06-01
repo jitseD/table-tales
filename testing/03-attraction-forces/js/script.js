426c6f01d5340b9e5cdc653cff593d1830be0920
Jitse Dekeyser <jitse.dekeyser@student.howest.be>
2024-06-01 17:11:47 +0200

    attraction-forces: vector class

--- testing/03-attraction-forces/js/script.js
+++ testing/03-attraction-forces/js/script.js
@@ -1,6 +1,6 @@
 const $canvas = document.querySelector(`.canvas`);
 const canvas = { ctx: null, height: innerHeight, width: innerWidth };
-let square = { x: 50, y: 50, size: 50, dx: 5, dy: 5, fill: `black` };
+let square = { pos: null, vel: null, size: 50, fill: `black` };
 
 const createCanvas = () => {
     canvas.ctx = $canvas.getContext(`2d`);
@@ -10,26 +10,41 @@ const createCanvas = () => {
     canvas.ctx.scale(scale, scale);
     $canvas.style.width = `${canvas.width}px`;
     $canvas.style.height = `${canvas.height}px`;
-    animateSquare();
 }
 
 const animateSquare = () => {
     canvas.ctx.clearRect(0, 0, canvas.width, canvas.height);
 
     canvas.ctx.fillStyle = square.fill;
-    canvas.ctx.fillRect(square.x, square.y, square.size, square.size);
+    canvas.ctx.fillRect(square.pos.x, square.pos.y, square.size, square.size);
 
-    square.x += square.dx;
-    square.y += square.dy;
+    square.pos.x += square.vel.x;
+    square.pos.y += square.vel.y;
 
-    if (square.x + square.size > canvas.width || square.x < 0) square.dx *= -1;
-    if (square.y + square.size > canvas.height || square.y < 0) square.dy *= -1;
+    if (square.pos.x + square.size > canvas.width || square.pos.x < 0) square.vel.x *= -1;
+    if (square.pos.y + square.size > canvas.height || square.pos.y < 0) square.vel.y *= -1;
 
     requestAnimationFrame(animateSquare);
 }
 
+class Vector {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    add(vector) {
+        this.x = this.x + vector.x;
+        this.y = this.y + vector.y;
+    }
+}
+
 const init = () => {
     createCanvas();
+    square.pos = new Vector(50, 50);
+    square.vel = new Vector(5, 5);
+
+    animateSquare();
 }
 
 init();
