700942f8da522460ea5c81826c60d7a8e1a1e6f9
Jitse Dekeyser <jitse.dekeyser@student.howest.be>
2024-06-01 17:20:46 +0200

    attraction-forces: mover class

--- testing/03-attraction-forces/js/script.js
+++ testing/03-attraction-forces/js/script.js
@@ -1,6 +1,10 @@
 const $canvas = document.querySelector(`.canvas`);
 const canvas = { ctx: null, height: innerHeight, width: innerWidth };
-let square = { pos: null, vel: null, size: 50, fill: `black` };
+let square;
+
+const randomNumber = (min, max) => {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
 
 const createCanvas = () => {
     canvas.ctx = $canvas.getContext(`2d`);
@@ -15,14 +19,9 @@ const createCanvas = () => {
 const animateSquare = () => {
     canvas.ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-    canvas.ctx.fillStyle = square.fill;
-    canvas.ctx.fillRect(square.pos.x, square.pos.y, square.size, square.size);
-
-    square.pos.x += square.vel.x;
-    square.pos.y += square.vel.y;
-
-    if (square.pos.x + square.size > canvas.width || square.pos.x < 0) square.vel.x *= -1;
-    if (square.pos.y + square.size > canvas.height || square.pos.y < 0) square.vel.y *= -1;
+    square.update();
+    square.checkEdges();
+    square.show();
 
     requestAnimationFrame(animateSquare);
 }
@@ -39,10 +38,33 @@ class Vector {
     }
 }
 
+class Mover {
+    constructor() {
+        this.size = 50;
+        this.pos = new Vector(randomNumber(0, canvas.width - this.size), randomNumber(0, canvas.height - this.size));
+        this.vel = new Vector(randomNumber(2, 5), randomNumber(2, 5));
+        this.fill = `black`
+    }
+
+    update() {
+        this.pos.add(this.vel);
+    }
+
+    show() {
+        canvas.ctx.fillStyle = this.fill;
+        canvas.ctx.fillRect(this.pos.x, this.pos.y, this.size, this.size);
+    }
+
+    checkEdges() {
+        if (this.pos.x + this.size > canvas.width || this.pos.x < 0) this.vel.x *= -1;
+        if (this.pos.y + this.size > canvas.height || this.pos.y < 0) this.vel.y *= -1;
+    }
+}
+
 const init = () => {
     createCanvas();
-    square.pos = new Vector(50, 50);
-    square.vel = new Vector(5, 5);
+    square = new Mover();
+    console.log(square);
 
     animateSquare();
 }
