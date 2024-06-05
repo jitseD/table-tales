eb24e1fad2eb8ea020af9006d33c65eb5809201a
Jitse Dekeyser <jitse.dekeyser@student.howest.be>
2024-06-05 10:26:05 +0200

    multi-phone-screen: roomCode from URL

--- testing/04-multi-phone-screen/public/js/script.js
+++ testing/04-multi-phone-screen/public/js/script.js
@@ -1,12 +1,50 @@
 const $myId = document.querySelector(`.my__id`);
+const $roomCode = document.querySelector(`.room__code`);
+const $isHost = document.querySelector(`.is__host`);
+const $otherIds = document.querySelector(`.other__ids`);
 
 let socket;
+let roomCode;
+
+const getUrlParameter = (name) => {
+    name = name.replace(/[\[]/, `\\[`).replace(/[\]]/, `\\]`);
+    const regex = new RegExp(`[\\?&]` + name + `=([^&#]*)`);
+    const results = regex.exec(location.search);
+    return results === null ? false : decodeURIComponent(results[1].replace(/\+/g, ` `));
+}
+
 
 const init = () => {
+    roomCode = getUrlParameter(`room`);
+    $roomCode.textContent = roomCode;
+
     socket = io.connect(`/`);
     socket.on(`connect`, () => {
         $myId.textContent = socket.id;
+        socket.emit(`connectToRoom`, roomCode);
     });
+
+    socket.on(`room`, (room) => {
+        if (room.host === socket.id) {
+            $isHost.textContent = `I am the room host`;
+        } else {
+            $isHost.textContent = `I am NOT the room host`;
+        }
+
+        console.log(room);
+        const clientIds = Object.keys(room.clients);
+        $otherIds.innerHTML = ``;
+        for (const otherSocetId in clientIds) {
+            if (clientIds.hasOwnProperty(otherSocetId)) {
+                const clientId = clientIds[otherSocetId]
+                if (clientId !== socket.id) {
+                    const listItem = document.createElement(`li`);
+                    listItem.textContent = clientId;
+                    $otherIds.appendChild(listItem);
+                }
+            }
+        }
+    })
 };
 
 init();
--- testing/04-multi-phone-screen/server.js
+++ testing/04-multi-phone-screen/server.js
@@ -14,6 +14,7 @@ const { Server } = require("socket.io");
 const io = new Server(server);
 
 const port = 443;
+const rooms = {};
 
 app.use(express.static('public'))
 app.use('/node_modules', express.static('node_modules'));
@@ -21,7 +22,39 @@ server.listen(port, () => {
     console.log(`App listening on port ${port}`)
 })
 
+// ----- socket room ----- //
+const addClientToRoom = (code, clientId) => {
+    if (!rooms[code]) {
+        rooms[code] = { clients: {}, host: clientId };
+    }
+    rooms[code].clients[clientId] = clientId;
+}
+const removeClientFromRoom = (code, clientId) => {
+    delete rooms[code].clients[clientId];
+    if (Object.keys(rooms[code].clients).length === 0) {
+        delete rooms[code];
+    } else {
+        const roomClients = Object.keys(rooms[code].clients);
+        if (rooms[code].host === clientId) {
+            rooms[code].host = roomClients[0];
+        }
+    }
+}
+
 io.on('connection', socket => {
     console.log(`Connection`);
 
+    socket.on(`connectToRoom`, (code) => {
+        socket.join(code);
+        addClientToRoom(code, socket.id);
+        io.to(code).emit('room', rooms[code]);
+    })
+
+    socket.on(`disconnect`, () => {
+        for (const code in rooms) {
+            socket.leave(code);
+            removeClientFromRoom(code, socket.id);
+            io.to(code).emit('room', rooms[code]);
+        }
+    });
 });
