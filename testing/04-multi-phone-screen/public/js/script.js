9c410575e0111f84caab0a0a1d1052320dbaf9ce
Jitse Dekeyser <jitse.dekeyser@student.howest.be>
2024-06-05 10:30:08 +0200

    multi-phone-screen: square animation

--- testing/04-multi-phone-screen/public/js/script.js
+++ testing/04-multi-phone-screen/public/js/script.js
@@ -2,9 +2,13 @@ const $myId = document.querySelector(`.my__id`);
 const $roomCode = document.querySelector(`.room__code`);
 const $isHost = document.querySelector(`.is__host`);
 const $otherIds = document.querySelector(`.other__ids`);
+const $canvas = document.querySelector(`.canvas`);
 
 let socket;
 let roomCode;
+const screenDimensions = { height: innerHeight, width: innerWidth };
+const canvas = { ctx: null, height: innerHeight, width: innerWidth };
+let square = { x: 50, y: 50, size: 50, dx: 2, dy: 2, fill: `black` };
 
 const getUrlParameter = (name) => {
     name = name.replace(/[\[]/, `\\[`).replace(/[\]]/, `\\]`);
@@ -13,8 +17,34 @@ const getUrlParameter = (name) => {
     return results === null ? false : decodeURIComponent(results[1].replace(/\+/g, ` `));
 }
 
+// ----- canvas ----- //
+const createCanvas = () => {
+    canvas.ctx = $canvas.getContext(`2d`);
+    const scale = window.devicePixelRatio;
+    $canvas.width = Math.floor(canvas.width * scale);
+    $canvas.height = Math.floor(canvas.height * scale);
+    canvas.ctx.scale(scale, scale);
+    $canvas.style.width = `${canvas.width}px`;
+    $canvas.style.height = `${canvas.height}px`;
+}
+const animateSquare = () => {
+    canvas.ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    canvas.ctx.fillStyle = square.fill;
+    canvas.ctx.fillRect(square.x, square.y, square.size, square.size);
+
+    square.x += square.dx;
+    square.y += square.dy;
+
+    if (square.x + square.size > canvas.width || square.x < 0) square.dx *= -1;
+    if (square.y + square.size > canvas.height || square.y < 0) square.dy *= -1;
+
+    requestAnimationFrame(animateSquare);
+}
 
 const init = () => {
+    createCanvas();
+
     roomCode = getUrlParameter(`room`);
     $roomCode.textContent = roomCode;
 
@@ -45,6 +75,8 @@ const init = () => {
             }
         }
     })
+
+    animateSquare();
 };
 
 init();
