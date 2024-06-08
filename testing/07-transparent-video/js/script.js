50736933ae1dbe0c40e9721e01411d03ccd37889
Jitse Dekeyser <jitse.dekeyser@student.howest.be>
2024-06-08 19:22:56 +0200

    transparent-video: check video format support

--- testing/07-transparent-video/js/script.js
+++ testing/07-transparent-video/js/script.js
@@ -1,5 +1,19 @@
-const init = () => {
+const $videoPlayer = document.querySelector(`.video__player`);
+const $videoType = document.querySelector(`.video__type`);
 
+const checkVideoSupport = (type, codecs) => {
+    return $videoPlayer.canPlayType(`video/${type}; codecs="${codecs}"`) !== ""
+}
+
+const adjustVideoFormat = (format) => {
+    $videoPlayer.src = `./assets/vid/test-${format}`;
+    $videoType.textContent = format;
+}
+
+const init = () => {
+    if (checkVideoSupport(`mp4`, `hvc1`)) adjustVideoFormat(`hecv.mov`);
+    else if (checkVideoSupport(`webm`, `vp9, vorbis`)) adjustVideoFormat(`webm.webm`);
+    else $videoType.textContent = `no browser support`;
 }
 
 init();
