47f0b0f8a3212b59e6fd3be390be46edd0284a7f
Jitse Dekeyser <jitse.dekeyser@student.howest.be>
2024-06-08 11:40:04 +0200

    fullscreen: go fullscreen

--- testing/05-fullscreen/js/script.js
+++ testing/05-fullscreen/js/script.js
@@ -1,4 +1,13 @@
+const $fullscreen = document.querySelector(`.fullscreen`);
+
+const fullscreenHandle = e => {
+    document.documentElement.requestFullscreen().catch((e) => {
+        console.error(e);
+    })
+}
+
 const init = () => {
+    $fullscreen.addEventListener(`click`, fullscreenHandle);
 }
 
 init();
