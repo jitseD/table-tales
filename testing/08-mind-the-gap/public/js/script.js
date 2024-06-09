8afc1ea7758055bc082d6ef64cda1ec9f8c5ee0e
Jitse Dekeyser <jitse.dekeyser@student.howest.be>
2024-06-09 11:42:59 +0200

    mind-the-gap: setup

---
+++ testing/08-mind-the-gap/public/js/script.js
@@ -0,0 +1,313 @@
+const $myId = document.querySelector(`.my__id`);
+const $roomCode = document.querySelector(`.room__code`);
+const $isHost = document.querySelector(`.is__host`);
+const $swipe = document.querySelector(`.swipe`);
+const $otherIds = document.querySelector(`.other__ids`);
+const $canvas = document.querySelector(`.canvas`);
+
+let socket;
+let roomCode;
+let roomHost = false;
+let isAnimating = false;
+let myCoords;
+let otherCoords;
+let lastTimeSent = 0;
+let animationFrameId;
+const swipe = { start: { x: null, y: null }, end: { x: null, y: null }, angle: null, isSwiping: false, isMouseDown: false, }
+const screenDimensions = { height: innerHeight, width: innerWidth };
+const canvas = { ctx: null, height: innerHeight, width: innerWidth };
+let square = { x: 50, y: 50, size: 50, dx: 2, dy: 2, fill: `black` };
+
+// ----- calculation functions ----- //
+const getUrlParameter = (name) => {
+    name = name.replace(/[\[]/, `\\[`).replace(/[\]]/, `\\]`);
+    const regex = new RegExp(`[\\?&]` + name + `=([^&#]*)`);
+    const results = regex.exec(location.search);
+    return results === null ? false : decodeURIComponent(results[1].replace(/\+/g, ` `));
+}
+const getExtremeCoords = (coords) => {
+    let minX = Infinity;
+    let minY = Infinity;
+    let maxX = -Infinity;
+    let maxY = -Infinity;
+
+    coords.forEach(coord => {
+        if (coord.x < minX) minX = coord.x;
+        if (coord.y < minY) minY = coord.y;
+        if (coord.x > maxX) maxX = coord.x;
+        if (coord.y > maxY) maxY = coord.y;
+    });
+
+    return { minX, minY, maxX, maxY };
+}
+const positionCanvas = (rotation, coords) => {
+    const { minX, minY, maxX, maxY } = getExtremeCoords(coords);
+
+    $canvas.style.width = `${canvas.width}px`;
+    $canvas.style.height = `${canvas.height}px`;
+    $canvas.style.rotate = `${rotation}deg`;
+    document.querySelector(`.screenRotation`).textContent = rotation;
+    document.querySelector(`.screenPos`).textContent = `minX: ${minX}, minY: ${minY}, maxX: ${maxX}, maxY: ${maxY}`;
+
+    switch (rotation) {
+        case 0:
+            $canvas.style.left = `${-minX}px`
+            $canvas.style.top = `${-minY}px`;
+            break;
+        case 90:
+            $canvas.style.left = `${screenDimensions.width + minY}px`
+            $canvas.style.top = `${-minX}px`;
+            break;
+        case 180:
+            $canvas.style.left = `${screenDimensions.width + minX}px`
+            $canvas.style.top = `${screenDimensions.height + minY}px`;
+            break;
+        case 270:
+            $canvas.style.top = `${screenDimensions.height + minX}px`
+            $canvas.style.left = `${-minY}px`;
+            break;
+        default:
+            $canvas.style.left = `${-minX}px`
+            $canvas.style.top = `${-minY}px`;
+            break;
+    }
+}
+const clampValue = (value, min, max) => {
+    return Math.max(min, Math.min(max, value))
+};
+
+
+// ----- canvas ----- //
+const createCanvas = () => {
+    canvas.ctx = $canvas.getContext(`2d`);
+    const scale = window.devicePixelRatio;
+    $canvas.width = Math.floor(canvas.width * scale);
+    $canvas.height = Math.floor(canvas.height * scale);
+    canvas.ctx.scale(scale, scale);
+    $canvas.style.width = `${canvas.width}px`;
+    $canvas.style.height = `${canvas.height}px`;
+}
+const animateSquare = (time) => {
+    canvas.ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    canvas.ctx.fillStyle = square.fill;
+    canvas.ctx.fillRect(square.x, square.y, square.size, square.size);
+
+    square.x += square.dx;
+    square.y += square.dy;
+
+    if (square.x + square.size > canvas.width || square.x < 0) square.dx *= -1;
+    if (square.y + square.size > canvas.height || square.y < 0) square.dy *= -1;
+
+    const diff = time - lastTimeSent;
+    if (diff > 1000) {
+        lastTimeSent = time;
+        socket.emit(`showSquare`, roomCode, square);
+    }
+
+    showConnectionLines();
+    animationFrameId = requestAnimationFrame(animateSquare);
+}
+
+// ----- side connections ----- //
+const showConnectionLines = () => {
+    const connectionLines = [];
+    const screenA = getExtremeCoords(myCoords);
+
+    for (let i = 0; i < otherCoords.length; i++) {
+        const screenB = getExtremeCoords(otherCoords[i]);
+
+        const line = getConnectionLine(screenA, screenB);
+        if (line) connectionLines.push(line);
+    }
+
+    connectionLines.forEach(line => {
+        canvas.ctx.strokeStyle = `black`;
+        canvas.ctx.lineWidth = 20;
+        canvas.ctx.beginPath();
+        canvas.ctx.moveTo(line.x1, line.y1);
+        canvas.ctx.lineTo(line.x2, line.y2);
+        canvas.ctx.stroke();
+    });
+}
+const getConnectionLine = (screenA, screenB) => {
+    const tolerance = 50;
+
+    if (Math.abs(screenA.minX - screenB.maxX) <= tolerance) return {            // leftArightB
+        x1: screenA.minX, y1: Math.max(screenA.minY, screenB.minY),
+        x2: screenA.minX, y2: Math.min(screenA.maxY, screenB.maxY)
+    }
+
+    if (Math.abs(screenA.maxX - screenB.minX) <= tolerance) return {            // rightAleftB
+        x1: screenA.maxX, y1: Math.max(screenA.minY, screenB.minY),
+        x2: screenA.maxX, y2: Math.min(screenA.maxY, screenB.maxY)
+    }
+
+    if (Math.abs(screenA.minY - screenB.maxY) <= tolerance) return {            // topAbottomB
+        x1: Math.max(screenA.minX, screenB.minX), y1: screenA.minY,
+        x2: Math.min(screenA.maxX, screenB.maxX), y2: screenA.minY
+    }
+
+    if (Math.abs(screenA.maxY - screenB.minY) <= tolerance) return {            //bottomAtopB
+        x1: Math.max(screenA.minX, screenB.minX), y1: screenA.maxY,
+        x2: Math.min(screenA.maxX, screenB.maxX), y2: screenA.maxY
+    }
+
+    return null;
+}
+
+// ----- mouse events ----- //
+const handleMouseDown = e => {
+    swipe.start = { x: e.clientX, y: e.clientY }
+    swipe.isMouseDown = true;
+}
+const handleMouseMove = e => {
+    if (!swipe.isMouseDown) return;
+    swipe.isSwiping = true;
+}
+const handleMouseUp = e => {
+    swipe.isMouseDown = false;
+    if (!swipe.isSwiping) return;
+
+    swipe.end = {
+        x: clampValue(e.clientX, 0, screenDimensions.width),
+        y: clampValue(e.clientY, 0, screenDimensions.height),
+    }
+
+    determineSwipeAngle();
+    handleSwipe();
+
+    swipe.isSwiping = false;
+}
+
+// ----- touch events ----- //
+const handleTouchStart = e => {
+    e.preventDefault();
+    swipe.start = { x: e.touches[0].clientX, y: e.touches[0].clientY }
+}
+const handleTouchMove = e => {
+    e.preventDefault();
+    swipe.isSwiping = true;
+}
+const handleTouchEnd = e => {
+    e.preventDefault();
+    if (!swipe.isSwiping) return;
+
+    swipe.end = {
+        x: clampValue(e.changedTouches[0].clientX, 0, screenDimensions.width),
+        y: clampValue(e.changedTouches[0].clientY, 0, screenDimensions.height),
+    }
+
+    determineSwipeAngle();
+    handleSwipe();
+
+    swipe.isSwiping = false;
+}
+
+// ----- swipe ----- //
+const determineSwipeAngle = () => {
+    const deltaX = swipe.end.x - swipe.start.x;
+    const deltaY = swipe.end.y - swipe.start.y;
+
+    if (Math.abs(deltaX) > Math.abs(deltaY)) swipe.angle = deltaX > 0 ? 0 : 180;            // horizontal
+    else swipe.angle = deltaY > 0 ? 90 : 270;                                               // vertical
+
+}
+const handleSwipe = () => {
+    document.querySelector(`.swipe`).textContent = `x: ${swipe.end.x}, y: ${swipe.end.y}, a: ${swipe.angle}`
+
+    const data = { x: swipe.end.x, y: swipe.end.y, angle: swipe.angle };
+    console.log('Swiped in direction:', data);
+
+    socket.emit('swipe', roomCode, data, Date.now());
+}
+
+const requestWakeLock = async () => {
+    if ('wakeLock' in navigator) {
+        try {
+            const wakeLock = await navigator.wakeLock.request('screen');
+        } catch (err) {
+            if (err.name != 'NotAllowedError') console.error(`${err.name}, ${err.message}`);
+        }
+    }
+}
+
+const init = () => {
+    createCanvas();
+
+    roomCode = getUrlParameter(`room`);
+    $roomCode.textContent = roomCode;
+
+    socket = io.connect(`/`);
+    socket.on(`connect`, () => {
+        $myId.textContent = socket.id;
+        socket.emit(`connectToRoom`, roomCode, screenDimensions);
+    });
+
+    socket.on(`room`, (room) => {
+        if (room.host === socket.id) {
+            $isHost.textContent = `I am the room host`;
+        } else {
+            $isHost.textContent = `I am NOT the room host`;
+        }
+
+        const clientIds = Object.keys(room.clients);
+        $otherIds.innerHTML = ``;
+        for (const otherSocetId in clientIds) {
+            if (clientIds.hasOwnProperty(otherSocetId)) {
+                const clientId = clientIds[otherSocetId]
+                if (clientId !== socket.id) {
+                    const listItem = document.createElement(`li`);
+                    listItem.textContent = clientId;
+                    $otherIds.appendChild(listItem);
+                }
+            }
+        }
+    })
+
+    // ----- mouse events ----- //
+    $canvas.addEventListener('mousedown', handleMouseDown);
+    $canvas.addEventListener('mousemove', handleMouseMove);
+    $canvas.addEventListener('mouseup', handleMouseUp);
+
+    // ----- touch events ----- //
+    $canvas.addEventListener('touchstart', handleTouchStart);
+    $canvas.addEventListener('touchmove', handleTouchMove);
+    $canvas.addEventListener('touchend', handleTouchEnd);
+
+    // ----- update canvas ----- //
+    socket.on(`updateCanvas`, (room) => {
+        otherCoords = [];
+        Object.values(room.clients).map((client) => {
+            if (client.id === socket.id) myCoords = client.coords;
+            else return otherCoords.push(client.coords);
+        });
+
+        canvas.width = room.canvas.width;
+        canvas.height = room.canvas.height;
+        createCanvas();
+
+        positionCanvas(room.clients[socket.id].rotation, myCoords);
+
+        if (animationFrameId) cancelAnimationFrame(animationFrameId);
+        if (room.host === socket.id) {
+            roomHost = true;
+            animationFrameId = requestAnimationFrame(animateSquare);
+            isAnimating = true;
+        } else {
+            roomHost = false;
+        }
+    })
+
+    socket.on(`showSquare`, (data) => {
+        if (!roomHost) {
+            square = data;
+            if (animationFrameId) cancelAnimationFrame(animationFrameId);
+            animationFrameId = requestAnimationFrame(animateSquare);
+        }
+    })
+
+    requestWakeLock();
+};
+
+init();
---
+++ testing/08-mind-the-gap/server.js
@@ -0,0 +1,230 @@
+const express = require(`express`);
+const app = express();
+const https = require(`https`);
+const fs = require(`fs`);
+
+const options = {
+    key: fs.readFileSync(`localhost.key`),
+    cert: fs.readFileSync(`localhost.crt`)
+}
+
+const server = https.createServer(options, app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+
+const port = 443;
+const rooms = {};
+let swipeEvents = [];
+const timestampThreshold = 5000;
+
+app.use(express.static(`public`))
+server.listen(port, () => {
+    console.log(`🙉 app listening on port ${port}`)
+})
+
+// ----- calculation functions ----- //
+const roundAngle = (angle) => {
+    const angleNormalized = ((angle % 360) + 360) % 360;
+
+    if (angleNormalized >= 0 && angleNormalized < 45) return 0;
+    if (angleNormalized >= 45 && angleNormalized < 135) return 90;
+    if (angleNormalized >= 135 && angleNormalized < 225) return 180;
+    if (angleNormalized >= 225 && angleNormalized < 315) return 270;
+
+    return 0;
+};
+const getScreenCoord = (i, screen) => {
+    switch (i) {
+        case 0: return { x: 0, y: 0 };                                        // Top-left
+        case 1: return { x: screen.width, y: 0 };                             // Top-right
+        case 2: return { x: screen.width, y: screen.height };                 // Bottom-right
+        case 3: return { x: 0, y: screen.height };                            // Bottom-left
+        default: return { x: 0, y: 0 };
+    }
+}
+const getExtremeCoords = (coords) => {
+    let minX = Infinity;
+    let minY = Infinity;
+    let maxX = -Infinity;
+    let maxY = -Infinity;
+
+    coords.forEach(coord => {
+        if (coord.x < minX) minX = coord.x;
+        if (coord.y < minY) minY = coord.y;
+        if (coord.x > maxX) maxX = coord.x;
+        if (coord.y > maxY) maxY = coord.y;
+    });
+
+    return { minX, minY, maxX, maxY };
+}
+
+// ----- socket room ----- //
+const addClientToRoom = (code, client) => {
+    const coords = [];
+    for (let i = 0; i < 4; i++) {
+        const coord = getScreenCoord(i, client);
+        coords.push(coord);
+    }
+    client.coords = coords;
+    client.rotation = 0;
+
+    if (!rooms[code]) {
+        rooms[code] = { clients: {}, canvas: { width: client.width, height: client.height }, host: client.id };
+    }
+    rooms[code].clients[client.id] = client;
+}
+const removeClientFromRoom = (code, clientId) => {
+    delete rooms[code].clients[clientId];
+    if (Object.keys(rooms[code].clients).length === 0) {
+        delete rooms[code];
+    } else {
+        const roomClients = Object.keys(rooms[code].clients);
+        if (rooms[code].host === clientId) {
+            rooms[code].host = roomClients[0];
+        }
+    }
+}
+
+// ----- swipe connecting ----- //
+const calculateSimultaneousSwipes = (code, latestSwipeEvent) => {
+    swipeEvents = swipeEvents.filter(swipeEvent => {
+        const timeDifferenceNow = Math.abs(swipeEvent.timestamp - latestSwipeEvent.timestamp);
+        return timeDifferenceNow <= timestampThreshold;
+    });
+    
+    const roomSwipeEvents = swipeEvents.filter(swipeEvent => {
+        return swipeEvent.code === code && swipeEvent.id !== latestSwipeEvent.id;
+    });
+
+
+    if (roomSwipeEvents.length > 0) {
+        for (let i = 0; i < roomSwipeEvents.length; i++) {
+            const timeDifference = Math.abs(roomSwipeEvents[i].timestamp - latestSwipeEvent.timestamp);
+            if (timeDifference <= timestampThreshold) {
+                const clientA = rooms[code].clients[swipeEvents[i].id];
+                const clientB = rooms[code].clients[latestSwipeEvent.id];
+
+                if (clientA && clientB) {
+                    const relPos = calculateRelPos(clientA, clientB, swipeEvents[i].data, latestSwipeEvent.data);
+                    rooms[code].clients[latestSwipeEvent.id].coords = relPos.coords;
+                    rooms[code].clients[latestSwipeEvent.id].rotation = (relPos.rotation + clientA.rotation + 180) % 360;;
+                    updateRoomCanvas(code);
+
+                    swipeEvents.splice(swipeEvents.indexOf(roomSwipeEvents[i]), 1);
+                    return;
+                }
+            }
+        }
+    }
+
+    swipeEvents.push(latestSwipeEvent);
+};
+const calculateRelPos = (clientA, clientB, swipeA, swipeB) => {
+    const angleDiff = roundAngle(swipeB.angle) - roundAngle(swipeA.angle);    // deg
+    const coords = [];
+
+    for (let i = 0; i < 4; i++) {
+        const coord = getScreenCoord(i, clientB);
+        const relCoord = calculateRelCoords(coord, angleDiff, clientA, clientB, swipeA, swipeB);
+        coords.push(relCoord);
+    }
+
+    return { coords, rotation: angleDiff };
+}
+const calculateRelCoords = (coord, angleDiff, clientA, clientB, swipeA, swipeB) => {
+    const { minX, minY } = getExtremeCoords(clientA.coords);
+    const centerAX = minX + clientA.width / 2;
+    const centerAY = minY + clientA.height / 2;
+    const centerBX = clientB.width / 2;
+    const centerBY = clientB.height / 2;
+
+    const angleDiffRad = (angleDiff - clientA.rotation) * (Math.PI / 180);                         // rad
+
+    // rotate screen B
+    let posX = (coord.x - centerBX) * Math.cos(angleDiffRad) - (coord.y - centerBY) * Math.sin(angleDiffRad) + centerBX;
+    let posY = (coord.x - centerBX) * Math.sin(angleDiffRad) + (coord.y - centerBY) * Math.cos(angleDiffRad) + centerBY;
+
+    posX = Math.round(posX);
+    posY = Math.round(posY);
+
+    // align to center of screen A
+    posX += centerAX - centerBX;
+    posY += centerAY - centerBY;
+
+    // move screen B by swipe A
+    const deltaAX = swipeA.x - clientA.width / 2
+    const deltaAY = swipeA.y - clientA.height / 2;
+
+    switch (clientA.rotation) {
+        case 90: posX += deltaAY; posY -= deltaAX; break;
+        case 180: posX -= deltaAX; posY -= deltaAY; break;
+        case 270: posX -= deltaAY; posY += deltaAX; break;
+        default: posX += deltaAX; posY += deltaAY; break;
+    }
+
+    // // move screen B by swipe B
+    const deltaBX = swipeB.x - centerBX;
+    const deltaBY = swipeB.y - centerBY;
+
+    switch (angleDiff + clientA.rotation) {
+        case 90: return { x: posX + deltaBY, y: posY - deltaBX };
+        case 180: return { x: posX - deltaBX, y: posY - deltaBY };
+        case 270: return { x: posX - deltaBY, y: posY + deltaBX };
+        default: return { x: posX + deltaBX, y: posY + deltaBY };
+    }
+}
+
+// ----- update canvas ------ //
+const updateRoomCanvas = (code) => {
+    const clients = Object.values(rooms[code].clients);
+    if (clients.length === 0) return;
+
+    const allCoords = [];
+    clients.forEach(client => {
+        allCoords.push(...client.coords);
+    });
+
+    const { minX, minY, maxX, maxY } = getExtremeCoords(allCoords);
+
+    const shiftX = minX;                                                      // shift coords so that lowest value is origin
+    const shiftY = minY;
+
+    clients.forEach(client => {
+        client.coords = client.coords.map(coord => ({
+            x: coord.x - shiftX,
+            y: coord.y - shiftY
+        }));
+    });
+
+    rooms[code].canvas = { width: maxX - minX, height: maxY - minY };
+    io.to(code).emit(`updateCanvas`, rooms[code]);
+}
+
+io.on(`connection`, socket => {
+    console.log(`✅ connection`);
+
+    socket.on(`connectToRoom`, (code, data) => {
+        socket.join(code);
+        data.id = socket.id;
+        addClientToRoom(code, data);
+        io.to(code).emit(`room`, rooms[code]);
+    });
+
+    socket.on(`swipe`, (code, data, timestamp) => {
+        const swipeEvent = { id: socket.id, code, data, timestamp };
+        calculateSimultaneousSwipes(code, swipeEvent);
+    });
+
+    socket.on(`showSquare`, (code, square) => {
+        io.to(code).emit(`showSquare`, square);
+    })
+
+    socket.on(`disconnect`, () => {
+        console.log(`❌ disconnection`);
+        for (const code in rooms) {
+            socket.leave(code);
+            removeClientFromRoom(code, socket.id);
+            io.to(code).emit(`room`, rooms[code]);
+        }
+    });
+});
